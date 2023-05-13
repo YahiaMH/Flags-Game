b002e3fdf3ef115d85254a745fb58c71b0c57a96
YahiaMH <79812056+YahiaMH@users.noreply.github.com>
2023-05-13 20:20:45 +0000

    removed php files

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,6 @@ function correctButton(difficulty) {
 	document.getElementsByTagName("footer")[0].innerHTML += "<br>CORRECT!";
 	changeColors();
 	streak++;
-	fetch("http://localhost:8000/addstreak.php", {method: "POST"});
 	updateStreak();
 	setTimeout(generateQuestion, 1500, difficulty);
 }
@@ -185,7 +184,7 @@ function generateQuestion(difficulty) {
 	}
 }
 
-function difficultyButtons() {	
+function difficultyButtons() {
 	document.getElementsByTagName('header')[0].innerHTML = "";
 	main.innerHTML =
 		"<section id='difficultyBtns'><button type='button' onclick='easyBtn()'>Easy</button><br><button type='button' onclick='mediumBtn()'>Medium</button><br><button type='button' onclick='hardBtn()'>Hard</button></section>";
@@ -195,7 +194,7 @@ function difficultyButtons() {
 const form = document.getElementsByTagName('form')[0];
 
 form.addEventListener('submit', function(event) {
-	
+
 	event.preventDefault();
 	difficultyButtons();
 });
